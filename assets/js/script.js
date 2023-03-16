e87fa93a3ee2f9dce1d5a855a07df581079cf103
alexanderscher <alexcscher@gmail.com>
2023-03-15 21:02:08 -0700

    finished setting up quiz, timer, and html displays

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,33 +18,107 @@ const questions = [
     correct: "b",
   },
 ];
+var wordBlank = document.querySelector(".word-blanks");
+var answerEls = document.querySelectorAll(".answer");
+var questionEl = document.getElementById("question");
+var startButton = document.querySelector(".start");
+var timerElement = document.querySelector(".timer");
+var correctAnswers = document.querySelector(".correct");
+var wrongAnswers = document.querySelector(".wrong");
+var timeScore = document.querySelector(".time");
+var results = document.querySelector(".results");
+var restartBtn = document.querySelector(".restart");
+var input = document.querySelector("input");
+var inputedName = document.querySelector(".name");
 
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
+var isWin = false;
+var winCounter = 0;
+var timer;
+var timerCount;
+var currentQ = 0;
+var storedName;
+timerCount = 30;
 
-let currentQuiz = 0;
-loadQuiz();
+function startQuiz() {
+  currentQ = 0;
+  winCounter = 0;
+  timerCount = 30;
+  correctAnswers.style.display = "none";
+  timeScore.style.display = "none";
+  questionEl.style.display = "inline";
+  timerElement.style.display = "inline";
+  timerElement.innerHTML = 30;
+  answerEls.forEach(function (el) {
+    el.style.display = "inline";
+  });
+  loadQuiz();
+  startTimer();
+}
 
 function loadQuiz() {
-  if (currentQuiz < questions.length) {
-    questionEl.innerText = questions[currentQuiz].question;
-    answerEls.forEach((el) => (el.innerText = questions[currentQuiz][el.id]));
+  console.log(currentQ);
+  if (currentQ < questions.length) {
+    questionEl.innerText = questions[currentQ].question;
+    answerEls.forEach(function (el) {
+      el.style.display = "block";
+      el.innerText = questions[currentQ][el.id];
+    });
   }
 }
 
 function btnClick(e) {
-  if (e.target.id === questions[currentQuiz].correct) {
+  if (e.target.id === questions[currentQ].correct) {
     alert("That's correct!");
-  } else alert("That's incorrect...");
-  currentQuiz++;
+    winCounter++;
+  } else {
+    alert("That's incorrect...");
+  }
+  currentQ++;
   loadQuiz();
 }
 
-answerEls.forEach(
-  (el) => el.addEventListener("click", btnClick)
-  // el.addEventListener("click", (e) =>
-  //   e.target.id === questions[currentQuiz].correct
-  //     ? alert("That's correct!");
-  //     : alert("That's incorrect...")
-  // )
-);
+answerEls.forEach(function (el) {
+  el.addEventListener("click", btnClick);
+});
+
+function finished() {
+  questionEl.style.display = "none";
+  timerElement.style.display = "none";
+  answerEls.forEach(function (el) {
+    el.style.display = "none";
+  });
+  input.style.display = "block";
+}
+
+function inputValue(e) {
+  if (e.key === "Enter") {
+    correctAnswers.style.display = "inline";
+    timeScore.style.display = "inline";
+    inputedName.style.display = "inline";
+
+    correctAnswers.textContent = winCounter;
+    timeScore.textContent = timerCount;
+    inputedName.textContent = input.value;
+    localName = input.value;
+    input.style.display = "none";
+    setScore();
+  }
+}
+
+function startTimer() {
+  timer = setInterval(function () {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (
+      (timerCount >= 0 && currentQ === questions.length) ||
+      timerCount === 0
+    ) {
+      clearInterval(timer);
+      finished();
+    }
+  }, 1000);
+}
+
+startButton.addEventListener("click", startQuiz);
+
+input.addEventListener("keypress", inputValue);
